141eeecbafd20100cd7e08c3cdeed4d7754d55b5
Ox Cart <ox.to.a.cart@gmail.com>
2016-08-29 15:48:01 -0500

    Added CONCAT

---
+++ concat.go
@@ -0,0 +1,41 @@
+package goexpr
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Concat joins a list of values using the first as a delimiter.
+func Concat(exprs ...Expr) Expr {
+	return &concat{exprs[0], exprs[1:]}
+}
+
+type concat struct {
+	delim   Expr
+	wrapped []Expr
+}
+
+func (e *concat) Eval(params Params) interface{} {
+	delim := e.delim.Eval(params)
+	buf := &bytes.Buffer{}
+	for i, wrapped := range e.wrapped {
+		first := i == 0
+		if !first {
+			fmt.Fprint(buf, delim)
+		}
+		fmt.Fprint(buf, wrapped.Eval(params))
+	}
+	return buf.String()
+}
+
+func (e *concat) String() string {
+	buf := &bytes.Buffer{}
+	buf.WriteString("CONCAT(")
+	buf.WriteString(e.delim.String())
+	for _, wrapped := range e.wrapped {
+		buf.WriteString(",")
+		fmt.Fprint(buf, wrapped.String())
+	}
+	buf.WriteString(")")
+	return buf.String()
+}
---
+++ concat_test.go
@@ -0,0 +1,23 @@
+package goexpr
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConcat(t *testing.T) {
+	pipe := Constant("|")
+	a := Constant("a")
+	b := Constant("b")
+	c := Constant("c")
+
+	assert.Equal(t, "", Concat(pipe).Eval(nil))
+	assert.Equal(t, "a", Concat(pipe, a).Eval(nil))
+	assert.Equal(t, "a|b", Concat(pipe, a, b).Eval(nil))
+	assert.Equal(t, "a|b|c", Concat(pipe, a, b, c).Eval(nil))
+
+	assert.Equal(t, "CONCAT(|)", Concat(pipe).String())
+	assert.Equal(t, "CONCAT(|,a)", Concat(pipe, a).String())
+	assert.Equal(t, "CONCAT(|,a,b)", Concat(pipe, a, b).String())
+	assert.Equal(t, "CONCAT(|,a,b,c)", Concat(pipe, a, b, c).String())
+}
