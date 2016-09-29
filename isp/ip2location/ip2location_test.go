e1cd31560c91ba469c89e117168fecc4bdbb9b8f
Ox Cart <ox.to.a.cart@gmail.com>
2016-09-29 08:24:13 -0500

    Abstracted ISP lookup into ISPProvider

---
+++ isp/ip2location/ip2location_test.go
@@ -0,0 +1,46 @@
+package ip2location
+
+import (
+	"testing"
+
+	"github.com/getlantern/goexpr"
+	"github.com/getlantern/goexpr/isp"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	prov, err := NewProvider("ip2location_test_data.csv")
+	if err != nil {
+		panic(err)
+	}
+	isp.SetProvider(prov)
+}
+
+func TestConversion(t *testing.T) {
+	assert.Equal(t, "66.69.229.169", ipIntToString(ipStringToInt("66.69.229.169")))
+	assert.EqualValues(t, -1, ipStringToInt("13.342.23"))
+}
+
+func TestISPGoodIP(t *testing.T) {
+	assert.Equal(t, "Time Warner Cable Internet LLC", isp.ISP(goexpr.Constant("66.69.229.169")).Eval(nil))
+}
+
+func TestISPUnknownIP(t *testing.T) {
+	assert.Nil(t, isp.ISP(goexpr.Constant("104.131.61.10")).Eval(nil))
+}
+
+func TestISPInvalidIP(t *testing.T) {
+	assert.Nil(t, isp.ISP(goexpr.Constant("13.342.23")).Eval(nil))
+}
+
+func TestASNGoodIP(t *testing.T) {
+	assert.Equal(t, 11427, isp.ASN(goexpr.Constant("66.69.229.169")).Eval(nil))
+}
+
+func TestASNUnknownIP(t *testing.T) {
+	assert.Nil(t, isp.ASN(goexpr.Constant("104.131.61.10")).Eval(nil))
+}
+
+func TestASNInvalidIP(t *testing.T) {
+	assert.Nil(t, isp.ASN(goexpr.Constant("13.342.23")).Eval(nil))
+}
--- isp/isp.go
+++ isp/isp.go
@@ -2,90 +2,33 @@
 package isp
 
 import (
-	"bufio"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
 	"sync/atomic"
 
-	"github.com/Workiva/go-datastructures/trie/yfast"
 	"github.com/getlantern/goexpr"
 )
 
 var (
-	entries = &atomic.Value{}
+	provider atomic.Value
 )
 
-func init() {
-	entries.Store(newTree())
+// ISPProvider implements the actual looking up of ISP and ASN information.
+type ISPProvider interface {
+	// ISP looks up the name of the Internet Service Provider corresponding to the
+	// given ip.
+	ISP(ip string) (isp string, found bool)
+
+	// ASN looks up the Autonomous System Number corresponding to the given ip.
+	ASN(ip string) (asn int, found bool)
 }
 
-func newTree() *yfast.YFastTrie {
-	return yfast.New(uint32(0)) // 32 bit universe size for 32 bit IPv4
+// SetProvider sets the ISP data provider
+func SetProvider(prov ISPProvider) {
+	provider.Store(prov)
 }
 
-type entry struct {
-	start uint64
-	end   int64
-	asn   int
-	name  string
-}
-
-// Key implements the interface yfast.Entry
-func (e *entry) Key() uint64 {
-	return e.start
-}
-
-// Init initializes the isp package using a datafile from
-// https://lite.ip2location.com/database/ip-asn.
-func Init(datafile string) error {
-	file, err := os.Open(datafile)
-	if err != nil {
-		return fmt.Errorf("Unable to open datafile %v: %v", datafile, err)
-	}
-	defer file.Close()
-	in := bufio.NewReader(file)
-	csvIn := csv.NewReader(in)
-	var newEntries []yfast.Entry
-	for {
-		row, err := csvIn.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return fmt.Errorf("Unable to read data from %v: %v", datafile, err)
-		}
-		if len(row) != 5 {
-			return fmt.Errorf("Unexpected row length %v: %v", row, err)
-		}
-		start, err := strconv.ParseInt(row[0], 10, 64)
-		if err != nil {
-			return fmt.Errorf("Unable to parse start IP %v: %v", row[0], err)
-		}
-		end, err := strconv.ParseInt(row[1], 10, 64)
-		if err != nil {
-			return fmt.Errorf("Unable to parse end IP %v: %v", row[1], err)
-		}
-		asn, err := strconv.Atoi(row[3])
-		if err != nil {
-			return fmt.Errorf("Unable to parse asn %v: %v", row[3], err)
-		}
-		entry := &entry{
-			start: uint64(start),
-			end:   end,
-			asn:   asn,
-			name:  row[4],
-		}
-		newEntries = append(newEntries, entry)
-	}
-	tree := newTree()
-	tree.Insert(newEntries...)
-	entries.Store(tree)
-
-	return nil
+func getProvider() ISPProvider {
+	return provider.Load().(ISPProvider)
 }
 
 // ISP returns the ISP name for a given IPv4 address
@@ -101,11 +44,11 @@ func (e *isp) Eval(params goexpr.Params) interface{} {
 	_ip := e.ip.Eval(params)
 	switch ip := _ip.(type) {
 	case string:
-		ent, found := lookup(ip)
+		result, found := getProvider().ISP(ip)
 		if !found {
 			return nil
 		}
-		return strings.TrimSpace(ent.name)
+		return result
 	}
 	return nil
 }
@@ -127,68 +70,15 @@ func (e *asn) Eval(params goexpr.Params) interface{} {
 	_ip := e.ip.Eval(params)
 	switch ip := _ip.(type) {
 	case string:
-		ent, found := lookup(ip)
+		result, found := getProvider().ASN(ip)
 		if !found {
 			return nil
 		}
-		return ent.asn
+		return result
 	}
 	return nil
 }
 
 func (e *asn) String() string {
 	return fmt.Sprintf("ASN(%v)", e.ip)
-}
-
-func lookup(ip string) (*entry, bool) {
-	i := ipStringToInt(ip)
-	if i == -1 {
-		return nil, false
-	}
-	tree := entries.Load().(*yfast.YFastTrie)
-	_e := tree.Predecessor(uint64(i))
-	if _e == nil {
-		return nil, false
-	}
-	e, _ := _e.(*entry)
-	if e.end < i {
-		return nil, false
-	}
-	return e, true
-}
-
-// Below IP conversion functions taken from
-// https://groups.google.com/forum/#!topic/golang-nuts/v4eJ5HK3stI
-
-// Convert uint to string IP
-func ipIntToString(ipnr int64) string {
-	var bytes [4]byte
-	bytes[0] = byte(ipnr & 0xFF)
-	bytes[1] = byte((ipnr >> 8) & 0xFF)
-	bytes[2] = byte((ipnr >> 16) & 0xFF)
-	bytes[3] = byte((ipnr >> 24) & 0xFF)
-
-	return fmt.Sprintf("%d.%d.%d.%d", bytes[3], bytes[2], bytes[1], bytes[0])
-}
-
-// Convert net.IP to int64
-func ipStringToInt(ipnr string) int64 {
-	bits := strings.Split(ipnr, ".")
-	if len(bits) != 4 {
-		return -1
-	}
-
-	b0, _ := strconv.Atoi(bits[0])
-	b1, _ := strconv.Atoi(bits[1])
-	b2, _ := strconv.Atoi(bits[2])
-	b3, _ := strconv.Atoi(bits[3])
-
-	var sum int64
-
-	sum += int64(b0) << 24
-	sum += int64(b1) << 16
-	sum += int64(b2) << 8
-	sum += int64(b3)
-
-	return sum
 }
